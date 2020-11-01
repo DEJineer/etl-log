3a8b5fd3238182bf4d9ced6188fb98c092fe01e6
dej <eteilj@gmail.com>
2020-11-01 11:44:10 +0100

    modify the etl.py to meet requirement for airflow functions call.

--- etl.py
+++ etl.py
@@ -5,56 +5,78 @@ import datetime
 import sqlalchemy
 
 
-# Load the files
-root_dir = os.path.dirname(os.path.abspath(__file__))
-data_directory = os.path.join(root_dir, 'data')
-# list of filepaths
-file_path_list = []
+def extract():
+    """
+    A function to extract data from the folder and load it in to a dataframe
+    :return: a dataframe representing all the data
+    """
+    # Load the files
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    data_directory = os.path.join(root_dir, 'data')
+    # list of file paths
+    file_path_list = []
 
-# we walk through all subdirectories to find .json files
-for subdir, dirs, files in os.walk(data_directory):
-    for file in files:
-        file_path_list.append(os.path.join(subdir, file))
+    # we walk through all subdirectories to find .json files
+    for subdir, dirs, files in os.walk(data_directory):
+        for file in files:
+            file_path_list.append(os.path.join(subdir, file))
 
-# List of dataframes for each file
-dfs = [pd.read_json(x, lines=True) for x in file_path_list]
+    # List of dataframes for each file
+    dfs = [pd.read_json(x, lines=True) for x in file_path_list]
 
-# we concatenate all the dataframes in only one
-df = pd.concat(dfs, axis=0, ignore_index=True)
-df_valid = df.dropna()
-df_valid = df_valid[df_valid['page'] == 'NextSong']
+    # we concatenate all the dataframes in only one
+    df = pd.concat(dfs, axis=0, ignore_index=True)
+    return df
 
-# transform ts into week of the year
-weeks = df_valid.ts.apply(lambda x: datetime.datetime.fromtimestamp(x/1000.0).weekday())
-weeks
-df_valid['week'] = weeks
 
-# We divide in dataframes for each week
-weeks_unique = weeks.drop_duplicates()
+def transform(dataframe):
+    """
+    A function to transform the dataframe and meet the requirement listed in readme.md
+    :param dataframe: the data to transform(panda.DataFrame)
+    :return: the transformed dataframe
+    """
+    df_valid = dataframe.dropna()
+    df_valid = df_valid[df_valid['page'] == 'NextSong']
 
-df_per_week = {week:df_valid[df_valid['week'] == week] for week in weeks}
+    # transform ts into week of the year
+    weeks = df_valid.ts.apply(lambda x: datetime.datetime.fromtimestamp(x / 1000.0).weekday())
+    weeks
+    df_valid['week'] = weeks
 
-for week, df in df_per_week.items():
-    df_per_week[week] = df[['week', 'song', 'sessionId']]\
-        .groupby(['week', 'song'], as_index=False)\
-        .agg('count')\
-        .sort_values('sessionId', ascending=False)\
-        .head(1)
+    # We divide in dataframes for each week
+    weeks_unique = weeks.drop_duplicates()
 
-best_song_per_week = pd.concat([df for df in df_per_week.values()],
-                     axis=0, ignore_index=True)
+    df_per_week = {week: df_valid[df_valid['week'] == week] for week in weeks}
 
-USER = 'dej'
-PASSWORD = 'dej'
-HOST = 'localhost'
-PORT = '5432'
-DATABASE = 'etl_log'
+    for week, df in df_per_week.items():
+        df_per_week[week] = df[['week', 'song', 'sessionId']] \
+            .groupby(['week', 'song'], as_index=False) \
+            .agg('count') \
+            .sort_values('sessionId', ascending=False) \
+            .head(1)
 
-# Create the database engine
-conn_uri = 'postgresql://{}:{}@{}:{}/{}'.format(USER, PASSWORD, HOST, PORT, DATABASE)
-db_engine = sqlalchemy.create_engine(conn_uri)
+    best_song_per_week = pd.concat([df for df in df_per_week.values()],
+                                   axis=0, ignore_index=True)
+    return best_song_per_week
 
-# Load to best_song_per_week table in etl_log
-best_song_per_week.to_sql("best_song_per_week",
-                          db_engine,
-                          if_exists='replace')
+
+def load(dataframe):
+    """
+    A function to load the transformed dataframe in a postgresql table
+    :param dataframe: dataframe to load
+    :return:
+    """
+    user = 'dej'
+    password = 'dej'
+    host = 'localhost'
+    port = '5432'
+    database = 'etl_log'
+
+    # Create the database engine
+    conn_uri = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, database)
+    db_engine = sqlalchemy.create_engine(conn_uri)
+
+    # Load to best_song_per_week table in etl_log
+    dataframe.to_sql("best_song_per_week",
+                     db_engine,
+                     if_exists='replace')
